5d3517e19af4d12e6784b9d81a1a03ba88d6c87f
DrMarioNascimento <mariocnascimento@gmail.com>
2026-09-13 17:53:55 -0300

    Abre rota e estilo da Operação Protocolo Eferente.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { HashRouter, Navigate, Route, Routes, useParams } from "react-router-dom
 import Home from "@/pages/Home";
 import EscapeHub from "@/pages/EscapeHub";
 import EscapeRoom from "@/pages/EscapeRoom";
+import OperacaoProtocolo from "@/pages/OperacaoProtocolo";
 
 function EscapeRoomRoute() {
   const { cursoId = "", roomId = "" } = useParams();
@@ -14,6 +15,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/escape" element={<EscapeHub />} />
+        <Route path="/escape/:cursoId/protocolo-eferente" element={<OperacaoProtocolo />} />
         <Route path="/escape/:cursoId/:roomId" element={<EscapeRoomRoute />} />
         <Route path="/escape/:roomId" element={<EscapeRoomRoute />} />
         <Route path="*" element={<Navigate to="/" replace />} />
